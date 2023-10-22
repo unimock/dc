9c58d828900ce48eb289ce84d13f28e0dcb14e3c
unimock <unimock@gate-m.de>
2023-10-22 19:52:06 +0200

    Establish dc-hcloud.

--- tests/create-host-server-project.sh
+++ tests/create-host-server-project.sh
@@ -3,13 +3,10 @@ error_detect=0
 err_report() { echo "Error on line $1" ; error_detect=1 ; } ; trap 'err_report $LINENO' ERR
 ################################################################################
 # "dc"  ssh-key bei hetzner Hinterlegen
-# hcloud nach /opt/dc/external ???
-# yq     nach /opt/dc/external  
-# hcloud context create  dc  # creates: cat ~/.config/hcloud/cli.toml 
-# hcloud image    list
-# hcloud location list
-# hcloud type     list
-# hname="tescht"
+# dc-hcloud context create  dc  # creates: ~/.config/hcloud/cli.toml 
+# dc-hcloud image    list
+# dc-hcloud location list
+# dc-hcloud type     list
 ################################################################################
 DC_HOST="test-host"
 SERVER="karl"
@@ -18,7 +15,11 @@ PROJECT="test-project"
 if [ "$1" = "" -o "$1" = "create" ] ; then 
   dc config create host ${DC_HOST} test.intra # create new host with hostname test.intra 
   dc vserver assign      ${DC_HOST} hcloud "name=$SERVER" "init.type=cx11" "init.image=ubuntu-22.04" "init.location=fsn1" "init.ssh-key=dc"
-  #dc config yq host     ${DC_HOST} "-i '.host.vserver.hcoud.name = "hugo"'" 
+  #dc config yq host     ${DC_HOST} -i '.host.vserver.hcoud.name = "'$SERVER'"'        # dc-hcloud server      list
+  #dc config yq host     ${DC_HOST} -i '.host.vserver.hcoud.init.type = cx11'          # dc-hcloud server-type list
+  #dc config yq host     ${DC_HOST} -i '.host.vserver.hcoud.init.image = ubuntu-22.04' # dc-hcloud image       list
+  #dc config yq host     ${DC_HOST} -i '.host.vserver.hcoud.init.location = fsn1'      # dc-hcloud location    list
+  #dc config yq host     ${DC_HOST} -i '.host.vserver.hcoud.init.ssh-key = dc'         # dc-hcloud ssh-key     list
   dc vserver create      ${DC_HOST}            # create assigned hetzner cloud server
   dc vserver install     ${DC_HOST}            # install docker an do a dist-upgrade 
   dc vserver reboot      ${DC_HOST}            # reboot and and wait until docker ist available
